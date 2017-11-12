8915b08de2285758e8e12e76555787f21466cd1e
Thomas Conkwright <tmconkwright@gmail.com>
2017-11-12 15:05:44 -0500

    testing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171112142534) do
+ActiveRecord::Schema.define(version: 20171112195945) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string "title"
@@ -63,6 +63,13 @@ ActiveRecord::Schema.define(version: 20171112142534) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.integer "role"
   end
 
 end
