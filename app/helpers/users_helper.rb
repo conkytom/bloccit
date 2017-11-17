7ba79b14b7f8ac54e6bef750ba0ce9229ab802db
Thomas Conkwright <tmconkwright@gmail.com>
2017-11-16 20:34:01 -0500

    Assingment 31 completed

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,13 @@
 module UsersHelper
+    def user_has_posted?
+        @user.posts.count > 0
+    end
+
+    def user_has_commented?
+        @user.comments.count > 0
+    end
+
+    def user_has_favorites?
+        @user.favorites.count > 0
+    end
 end
